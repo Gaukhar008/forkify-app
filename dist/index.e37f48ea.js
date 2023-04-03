fd4927717b7ac5a9ae6c8b05dac055be4e1752e8
Gaukhar Abdizhapbarova <gauhar.abdizhapbarova@gmail.com>
2023-04-03 22:24:27 +0600

    Add event listeners to hashchange and load events

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -585,9 +585,12 @@ const renderSpinner = function(parentEl) {
 };
 const showRecipe = async function() {
     try {
+        const id = window.location.hash.slice(1);
+        console.log(id);
+        if (!id) return;
         // 1) Loading recipe
         renderSpinner(recipeContainer);
-        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
         let { recipe  } = data.data;
@@ -718,7 +721,8 @@ const showRecipe = async function() {
         alert(err);
     }
 };
-showRecipe();
+window.addEventListener("hashchange", showRecipe);
+window.addEventListener("load", showRecipe);
 
 },{"../img/icons.svg":"cMpiy","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","core-js/modules/es.regexp.flags.js":"gSXXb","core-js/modules/web.immediate.js":"49tUX","regenerator-runtime/runtime":"dXNgZ"}],"cMpiy":[function(require,module,exports) {
 module.exports = require("1cf12df6139d3c05").getBundleURL("hWUTQ") + "icons.21bad73c.svg" + "?" + Date.now();
