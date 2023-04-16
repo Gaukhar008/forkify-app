b510406ebeb2261dbd52f6e7ac32f200f6320d81
Gaukhar Abdizhapbarova <gauhar.abdizhapbarova@gmail.com>
2023-04-16 22:13:09 +0600

    Add functionality to update servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,6 +30,8 @@ const controlRecipes = async function () {
   }
 }
 
+
+
 const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
@@ -60,8 +62,17 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  // Update recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
